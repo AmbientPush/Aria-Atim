aca2fa1e0ece08df68341b65886b69e9a0be689a
AmbientPush <moscickibartek5@gmail.com>
2025-09-06 14:01:11 +0200

    Create main.js (mapping shit)

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+SaveFileReader.fallback.putAll(
+  //Quartz
+  "a-f-01-13-quartz-wall-ore", "a-f-01-13-quartz-ore-wall",
+
+  //Carbon
+
+  //floora
+  "a-f-06-00-carbon-floora", "a-f-05-01-carbon-floora",
+  "a-f-05-00-carbon-floora", "a-f-05-01-carbon-floora",
+
+  //floorb
+  "a-f-06-01-carbon-floorb", "a-f-05-02-carbon-floorb",
+  "a-f-05-01-carbon-floorb", "a-f-05-02-carbon-floorb",
+
+  //ore
+  "a-f-06-02-carbon-ore", "a-f-05-11-carbon-ore",
+  "a-f-05-02-carbon-ore", "a-f-05-11-carbon-ore",
+
+  //wall  content/blocks/environment/a-f-05-carbon/a-f-05-12-carbon-wall.json
+  "a-f-06-3-carbon-wall", "a-f-05-12-carbon-wall",
+  "a-f-05-3-carbon-wall", "a-f-05-12-carbon-wall",
+
+  //boulder  content/blocks/environment/a-f-05-carbon/a-f-05-15-carbon-boulder.json
+  "a-f-06-4-carbon-boulder", "a-f-05-15-carbon-boulder",
+  "a-f-05-4-carbon-boulder", "a-f-05-15-carbon-boulder",
+)
